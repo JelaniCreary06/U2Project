9649ba42c635b616faeae348d1626232f725d2de
BT_4N2_29 <user@user.com>
2022-10-12 12:32:31 -0400

    Added to LinearEquation | Runner completed.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -15,12 +15,53 @@ public class LinearEquation {
         return Math.sqrt((Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2)));
     }
 
+    public double distanceRounded() {
+        double num = Math.sqrt((Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2)));
+        String newNum = df.format(num);
+        double roundedNum = Double.parseDouble(newNum);
+
+        return roundedNum;
+    }
+
     public String distanceToString(String decimalOrFraction) {
+        String ds;
+        ds = "d = √[(" + this.x2 + " - " + this.x1 + ")^2 + (" + this.y2 + " - " + this.y1 + ")^2]";
+
+        //if (decimalOrFraction.toLowerCase() == "decimal" || decimalOrFraction.toLowerCase() == "d") {
+
+       // } else {
+
+       // }
+
+        return ds;
+    }
+
+    //public String distanceToString() {
+
+
+   // }
+
+    //  m=(y2-y1)/(x2-x1) - Slope Formula
+    public double slope() {
+        return (this.y2 - this.y1) / (this.x2 - this.x1);
+    }
+
+    public double slopeRounded() {
+        double num = (this.y2 - this.y1) / (this.x2 - this.x1);
+        String newNum = df.format(num);
+        double roundedNum = Double.parseDouble(newNum);
+
+        return roundedNum;
+    }
+/*
+    public String slopeToString(String decimalOrFraction) {
 
     }
 
-    public String distanceToString() {
-
+    public String slopeToString() {
 
     }
+
+    y -mx = b | y - slope(x) = b for yintercept
+*/
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,43 @@
+import javax.sound.sampled.Line;
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        double x1, y1, x2, y2;
+        String cOne, cTwo;
+
+        System.out.println("Hello!\nThis program takes 2 valid coordinates and does linear equations." +
+                "\nReady to begin?, input below!\nUse the (0,0) format.");
+
+        System.out.print("Coordinate one: ");
+        cOne = input.nextLine();
+
+        System.out.print("Coordinate two: ");
+        cTwo = input.nextLine();
+
+        x1 = coordinateOne(cOne); y1 = coordinateTwo(cOne); x2 = coordinateOne(cTwo); y2 = coordinateTwo(cTwo);
+
+        LinearEquation operator = new LinearEquation(x1, y1, x2, y2);
+
+        System.out.println(operator.distanceRounded());
+        System.out.println(operator.slopeRounded());
+    }
+
+    public static double coordinateOne(String coordinate) {
+        int c = coordinate.indexOf(",");
+        String number = coordinate.substring(1, c);
+
+        double convertedNumber = Integer.parseInt(number);
+        return convertedNumber;
+    }
+
+    public static double coordinateTwo(String coordinate) {
+        int c = coordinate.indexOf(",");
+        String number = coordinate.substring(c + 1, coordinate.length() - 1);
+
+        double convertedNumber = Integer.parseInt(number);
+        return convertedNumber;
     }
 }
